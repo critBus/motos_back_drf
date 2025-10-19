7e4c65ad08368e3e47e562501b8f4f24119ba2b6
René Lázaro Collado Arteaga <94305086+critBus@users.noreply.github.com>
2025-10-19 19:25:01 -0400

    simple

--- build_files.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Build the project
-echo "Building the project..."
-python3 -m pip install -r requirements.txt
-
-echo "Collect Static..."
-python3 manage.py collectstatic --noinput --clear
